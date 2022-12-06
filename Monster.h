912e924af00fda4917f07cd82019d297bfc98eb8
cdgarcia420 <cdgarcia20@my.trine.edu>
2022-12-06 17:05:57 -0500

    Commit 12/6/2022

--- Monster.h
+++ Monster.h
@@ -1,74 +1,74 @@
-//typedef std::string Name;
-//typedef double Weight;
-//typedef double Height;
-//typedef uint8_t EvoLevel;
-//typedef uint8_t DexID;
-//typedef uint8_t BSE_HP;
-//typedef uint8_t BSE_ATK;
-//typedef uint8_t BSE_DEF;
-//typedef uint8_t BSE_SPD;
-//typedef uint8_t BSE_SPA;
-//typedef uint8_t BSE_SPD;
-//typedef uint8_t BSE_SPE;
-//typedef uint8_t EXP_YLD;
-//
-//enum class BaseType {
-//	Bug,
-//	Dark,
-//	Dragon,
-//	Electric,
-//	Fairy,
-//	Fighting,
-//	Fire,
-//	Flying,
-//	Ghost,
-//	Grass,
-//	Ground,
-//	Ice,
-//	Normal,
-//	Poison,
-//	Psychic,
-//	Rock,
-//	Steel,
-//	Water
-//};
-//
-//class Type {
-//	Type(BasicType main);
-//	Type(BasicType main, BasicType sub);
-//	void addSubType(BasicType);
-//	bool hasType(BasicType) const;
-//};
-//
-//enum class Gender {
-//	Male,
-//	Female,
-//	Unknown
-//};
-//
-//enum class ExpGroup {
-//	Expidited,
-//	Normal,
-//	Paced
-//};
-//
-//class Pokemon {
-//	Pokemon() = delete;
-//	Pokemon(Name name,
-//		Type type,
-//		Weight, weight,
-//		Height height,
-//		Gender gender,
-//		EvoLevel level,
-//		bool canEvolve,
-//		DexID Pid,
-//		ExpGroup experience,
-//		BSE_HP hp,
-//		BSE_ATK atk,
-//		BSE_SPA spa,
-//		BSE_DEF def,
-//		BSE_SPD spd,
-//		BSE_SPE spe,
-//		EXP_YLD yld);
-//
-//};
+typedef std::string Name;
+typedef double Weight;
+typedef double Height;
+typedef uint8_t EvoLevel;
+typedef uint8_t DexID;
+typedef uint8_t BSE_HP;
+typedef uint8_t BSE_ATK;
+typedef uint8_t BSE_DEF;
+typedef uint8_t BSE_SPD;
+typedef uint8_t BSE_SPA;
+typedef uint8_t BSE_SPD;
+typedef uint8_t BSE_SPE;
+typedef uint8_t EXP_YLD;
+
+enum class BaseType {
+	Bug,
+	Dark,
+	Dragon,
+	Electric,
+	Fairy,
+	Fighting,
+	Fire,
+	Flying,
+	Ghost,
+	Grass,
+	Ground,
+	Ice,
+	Normal,
+	Poison,
+	Psychic,
+	Rock,
+	Steel,
+	Water
+};
+
+class Type {
+	Type(BasicType main);
+	Type(BasicType main, BasicType sub);
+	void addSubType(BasicType);
+	bool hasType(BasicType) const;
+};
+
+enum class Gender {
+	Male,
+	Female,
+	Unknown
+};
+
+enum class ExpGroup {
+	Expidited,
+	Normal,
+	Paced
+};
+
+class Pokemon {
+	Pokemon() = delete;
+	Pokemon(Name name,
+		Type type,
+		Weight, weight,
+		Height height,
+		Gender gender,
+		EvoLevel level,
+		bool canEvolve,
+		DexID Pid,
+		ExpGroup experience,
+		BSE_HP hp,
+		BSE_ATK atk,
+		BSE_SPA spa,
+		BSE_DEF def,
+		BSE_SPD spd,
+		BSE_SPE spe,
+		EXP_YLD yld);
+
+};
